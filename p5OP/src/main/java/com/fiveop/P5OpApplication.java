12b594a0a5e88d0caa7b55cfbbee16558db20cf8
FGlry <vietcao843@gmail.com>
2025-12-17 12:49:01 +0700

    first config

---
+++ p5OP/src/main/java/com/fiveop/P5OpApplication.java
@@ -0,0 +1,12 @@
+package com.fiveop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5OpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5OpApplication.class, args);
+	}
+}
